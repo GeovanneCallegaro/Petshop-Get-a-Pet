8218280da58ab9b07710df07776fc399799fed11
Geovanne <callegarog00@gmail.com>
2022-02-17 09:55:49 -0300

    validation userController function register

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,41 @@ const User = require('../models/User')
 module.exports = class UserController {
 
   static async register(req, res) {
-    res.json('Olá Get a Pet')
+    const {name, email, phone, password, confirmPassword} = req.body
+
+    // validations
+    if(!name) {
+      res.status(422).json({ message: 'O nome é obrigatório' })
+      return
+    }
+    if(!email) {
+      res.status(422).json({ message: 'O email é obrigatório' })
+      return
+    }
+    if(!phone) {
+      res.status(422).json({ message: 'O telefone é obrigatório' })
+      return
+    }
+    if(!password) {
+      res.status(422).json({ message: 'A senha é obrigatória' })
+      return
+    }
+    if(!confirmPassword) {
+      res.status(422).json({ message: 'A confirmação de senha é obrigatória' })
+      return
+    }
+
+    if(password !== confirmPassword) {
+      res.status(422).json({ message: 'As senhas devem ser iguais' })
+      return
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email})
+
+    if(userExists) {
+      res.status(422).json({ message: 'O email já está cadastrado!'})
+      return
+    }
   }
 }
