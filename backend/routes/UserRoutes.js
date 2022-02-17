86f7bf361627257a88f74394946fd6df9b2fe3e4
Geovanne <callegarog00@gmail.com>
2022-02-17 14:29:39 -0300

    jwt token verification and helper to secure route through middleware

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,9 +2,14 @@ const router = require('express').Router()
 
 const UserController = require('../controllers/UserController')
 
+// middleware
+const checkToken = require('../helpers/verify-token')
+
+
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', checkToken,  UserController.editUser)
 
 module.exports = router
