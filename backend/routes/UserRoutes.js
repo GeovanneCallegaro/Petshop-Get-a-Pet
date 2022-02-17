1d8f5b031ca213efb2d82eac16603c3438578cc5
Geovanne <callegarog00@gmail.com>
2022-02-17 15:37:23 -0300

    upload images using multer, persisting the data in db

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,12 +4,18 @@ const UserController = require('../controllers/UserController')
 
 // middleware
 const checkToken = require('../helpers/verify-token')
+const imageUpload = require('../helpers/image-upload')
 
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', checkToken,  UserController.editUser)
+router.patch(
+  '/edit/:id',
+  checkToken,
+  imageUpload.single("image"),
+  UserController.editUser
+)
 
 module.exports = router
