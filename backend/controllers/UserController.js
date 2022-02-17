936b3ea0dda3d0a41fb2eb248cde0ae8290d7b52
Geovanne <callegarog00@gmail.com>
2022-02-17 12:31:14 -0300

    taking the headers token and decoding

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
 
 module.exports = class UserController {
 
@@ -103,7 +105,12 @@ module.exports = class UserController {
     let currentUser
 
     if(req.headers.authorization) {
-      
+      const token = getToken(req)
+      const decoded = jwt.verify(token, "nossosecret")
+
+      currentUser = await User.findById(decoded.id)
+
+      currentUser.password = undefined
 
     } else {
       currentUser = null
