46f57eaba5bf3df9458ac1b5fe9b8110cf610621
Geovanne <callegarog00@gmail.com>
2022-02-18 10:46:37 -0300

    adding multiple images to pets

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -4,7 +4,13 @@ const PetController = require('../controllers/PetController')
 
 // middlewares
 const checkToken = require('../helpers/verify-token')
+const imageUpload = require('../helpers/image-upload')
 
-router.post('/create', checkToken, PetController.create)
+router.post(
+  '/create',
+  checkToken,
+  imageUpload.array('images'),
+  PetController.create
+  )
 
 module.exports = router
