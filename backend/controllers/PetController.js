5da99e237678479486e623cdc552e6d64f685e36
Geovanne <callegarog00@gmail.com>
2022-02-18 13:42:13 -0300

    route to remove a pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -110,6 +110,7 @@ module.exports = class PetController {
 
     const id = req.params.id
 
+    // check if id is valid
     if(!ObjectId.isValid(id)) {
       res.status(422).json({ message: 'ID inválido!'})
       return
@@ -127,6 +128,40 @@ module.exports = class PetController {
     res.status(200).json({
       pet: pet
     })
+  }
 
+  static async removePetById(req, res) {
+    const id = req.params.id
+
+    // check if id is valid
+    if(!ObjectId.isValid(id)) {
+      res.status(422).json({ message: 'ID inválido!'})
+      return
+    }
+
+    // check if pets exists
+    const pet = await Pet.findOne({_id: id})
+
+    if(!pet) {
+      res.status(404).json({
+        message: 'Pet não encontrado!'
+      })
+    }
+
+    // check if loggend in user registered the pet
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    if(pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({
+        message: 'Houve um problema em processar sua solicitação, tente novamente mais tarde! '
+      })
+    }
+
+    await Pet.findByIdAndRemove(id)
+
+    res.status(200).json({
+      message: 'Pet removido com sucesso!'
+    })
   }
 }
