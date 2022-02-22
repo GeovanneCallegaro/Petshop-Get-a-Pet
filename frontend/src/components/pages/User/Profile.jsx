2181b29886cdcfd093d33ba511fbeb8eadd16d04
Geovanne <callegarog00@gmail.com>
2022-02-22 16:05:57 -0300

    making edit user form

--- frontend/src/components/pages/User/Profile.jsx
+++ frontend/src/components/pages/User/Profile.jsx
@@ -1,7 +1,80 @@
+import { Input } from './../../form/Input';
+import formStyles from './../../form/Form.module.css'
+import styles from './Profile.module.css'
+import { useState, useEffect } from 'react';
+
+
+
 export function Profile() {
+  const [user, setUser] = useState({})
+
+  function handleChange() {
+
+  }
+
+  function onFileChange() {
+
+  }
+
   return (
     <section>
-      <h1>Profile</h1>
+      <div className={styles.profile_container}>
+        <h1>Perfil</h1>
+        <p>Preview Imagem</p>
+      </div>
+      <form className={formStyles.form_container}>
+        <Input
+          text='Imagem'
+          type='file'
+          name='image'
+          handleOnChange={onFileChange}
+        />
+
+        <Input
+          text='E-mail'
+          type='email'
+          name='email'
+          placeholder="Digite seu e-mail"
+          handleOnChange={handleChange}
+          value={user.email || ''}
+        />
+
+        <Input
+          text='Nome'
+          type='text'
+          name='name'
+          placeholder="Digite seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ''}
+        />
+
+        <Input
+          text='Telefone'
+          type='text'
+          name='phone'
+          placeholder="Digite seu telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ''}
+        />
+
+        <Input
+          text='Senha'
+          type='password'
+          name='password'
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+          value={user.password|| ''}
+        />
+
+        <Input
+          text='Confirme sua senha'
+          type='password'
+          name='confirmPassword'
+          placeholder="Confirme sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Editar"/>
+      </form>
     </section>
   )
 }
