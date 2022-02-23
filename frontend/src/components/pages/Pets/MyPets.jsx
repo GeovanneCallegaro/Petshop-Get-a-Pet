a7097f9a799b4f459cb43d1893bdf4b3ce351c98
Geovanne <callegarog00@gmail.com>
2022-02-23 13:11:31 -0300

    request to get the pets

--- frontend/src/components/pages/Pets/MyPets.jsx
+++ frontend/src/components/pages/Pets/MyPets.jsx
@@ -1,8 +1,28 @@
 import { useState, useEffect } from "react"
+
 import { Link } from 'react-router-dom';
+
+import { RoundedImage } from './../../layout/RoundedImage';
+import api from '../../../utils/api'
+
+/* hooks */
+import { useFlashMessage } from './../../../hooks/useFlashMessage';
 
 export function MyPets() {
   const [pets, setPets] = useState([])
+  const [token] = useState(localStorage.getItem('token' || ''))
+  const {setFlashMessage} = useFlashMessage()
+
+
+  useEffect(() => {
+    api.get('/pets/mypets', {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+    }).then((response) => {
+      setPets(response.data.pets)
+    })
+  }, [token])
 
   return (
     <section>
