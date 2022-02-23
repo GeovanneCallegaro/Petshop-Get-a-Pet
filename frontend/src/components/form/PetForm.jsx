72a74d0376e84d0953c65649ec7b9153d97032ba
Geovanne <callegarog00@gmail.com>
2022-02-23 10:32:50 -0300

    finalizing the form to register a pet

--- frontend/src/components/form/PetForm.jsx
+++ frontend/src/components/form/PetForm.jsx
@@ -11,20 +11,36 @@ export function PetForm({handleSubmit, petData, btnText}) {
   const colors = ["Branco", "Preto", "Cinza", "Caramelo", "Mesclado"]
 
   function onFileChange(e) {
-
+    setPreview(Array.from(e.target.files))
+    setPet({...pet, images: [...e.target.files]})
   }
 
   function handleChange(e) {
-
+    setPet({...pet, [e.target.name]: e.target.value})
   }
 
   function handleColor(e) {
-
+    setPet({...pet, color: e.target.options[e.target.selectedIndex].text})
   }
 
+  function submit(e) {
+    e.preventDefault()
+    console.log(pet)
+    // handleSubmit(pet)
+  }
 
   return (
-    <form className={formStyles.form_container}>
+    <form className={formStyles.form_container} onSubmit={submit}>
+      <div className={formStyles.preview_pet_images}>
+        {preview.length > 0
+          ? preview.map((image, index) => (
+            <img src={URL.createObjectURL(image)} alt={pet.name} key={`${pet.name}+${index}`}/>
+          )) :
+          pet.images && pet.images.map((image, index) => (
+            <img src={`http:localhost:5000/images/pets/${image}`} alt={pet.name} key={`${pet.name}+${index}`}/>
+          ))
+        }
+      </div>
       <Input
         text= 'Imagens do Pet'
         type= 'file'
