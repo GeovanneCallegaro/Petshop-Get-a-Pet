cc968cdac7c674f0a82ea2c98f0b4abf33aef0d6
Geovanne <callegarog00@gmail.com>
2022-02-23 13:53:57 -0300

    dashboard css

--- frontend/src/components/pages/Pets/MyPets.jsx
+++ frontend/src/components/pages/Pets/MyPets.jsx
@@ -27,25 +27,25 @@ export function MyPets() {
 
   return (
     <section>
-      <div>
+      <div  className={styles.petlist_header}>
         <h1>My Pets</h1>
         <Link to="/pet/add"> Cadastrar Pet</Link>
       </div>
-      <div>
+      <div className={styles.petlist_container}>
         {pets.length > 0 ?
           pets.map((pet) => (
-              <div key={pet._id}>
+              <div key={pet._id} className={styles.petlist_row}>
                 <RoundedImage
                   src={`http://localhost:5000/images/pets/${pet.images[0]}`}
                   alt={pet.name}
-                  width='75px'
+                  width='px75'
                 />
                   <span className="bold">{pet.name}</span>
-                  <div className={styles.action}>
+                  <div className={styles.actions}>
                     {pet.available ? (
                       <>
                         {pet.adopter && (
-                          <button>Concluir adoção</button>
+                          <button className={styles.conclude_btn}>Concluir adoção</button>
                         )}
                         <Link to={`/pet/edit${pet._id}`}>Editar</Link>
                         <button>Excluir</button>
