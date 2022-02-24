c9fb619f477fc70b131102e78366d77a2c984b39
Geovanne <callegarog00@gmail.com>
2022-02-24 12:56:57 -0300

    css pet details

--- frontend/src/components/pages/Pets/PetDetails.jsx
+++ frontend/src/components/pages/Pets/PetDetails.jsx
@@ -21,12 +21,12 @@ export function PetDetails() {
   return (
     <>
       {pet.name && (
-        <section>
-          <div>
-            <h1>Conhcendo o Pet: {pet.name}</h1>
+        <section className={styles.pet_details_container}>
+          <div className={styles.pet_details_header}>
+            <h1>Conhecendo o Pet: {pet.name}</h1>
             <p>Se tiver interesse, marque uma visita para conhecê-lo</p>
           </div>
-          <div>
+          <div className={styles.pet_images}>
             {pet.images.map((image, index) => (
               <img src={`http://localhost:5000/images/pets/${image}`} alt={pet.name} key={index}/>
             ))}
