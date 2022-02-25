e3a3b06c7684f5411820623b5ed7f1abf7c1339e
okatsuo <rafael.goncanna@gmail.com>
2022-02-25 18:11:52 -0300

    refactor: use db connection from env file instead hardcoded

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
 
+const mongodb_conn = () => {
+  if(!process.env.MONGODB_CONN) throw new Error('É necessário a conexão com o MongoDB no arquivo .env')
+  return process.env.MONGODB_CONN
+}
+
 async function main() {
-  await mongoose.connect('mongodb+srv://Geovanne:xouGMQZ0DLo8iOKg@cluster0.lhpb0.mongodb.net/Cluster0?retryWrites=true&w=majority')
+  await mongoose.connect(mongodb_conn())
   console.log('Conectou ao moongose')
 }
 
-main().catch((err) => console.log(err))
+main().catch((err) => console.error(err))
 
 module.exports = mongoose
 
